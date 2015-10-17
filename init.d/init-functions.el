b232d87de599d81911df455e33256d1365ca9c07
Morten Kristensen <msk@nullpointer.dk>
2015-10-17 23:33:06 +0200

    M-SPC cycles one space, no spaces and original spaces.

--- init.d/init-functions.el
+++ init.d/init-functions.el
@@ -7,6 +7,11 @@
 (global-set-key (kbd "C-c o") 'ff-find-other-file)
 (global-set-key (kbd "C-c w") 'whitespace-mode)
 
+;; Cycle through "just one space", "no spaces" and original number of spaces,
+;; instead of just "just one space". It does not delete newlines, too.
+(global-set-key (kbd "M-SPC")
+                '(lambda () (interactive) (cycle-spacing +1 t)))
+
 ;;;;;;;;; FUNCTIONS
 
 ;; convert current buffer to unix EOLs
