4425d32bf841b90263dc3f367351690db0ad51f3
Morten Kristensen <msk@nullpointer.dk>
2015-10-17 10:30:43 +0200

    Added custom faces/vars to init.el

--- init.el
+++ init.el
@@ -5,10 +5,27 @@
 ;; Constants.
 (defconst init-dir "~/.emacs.d/init.d")
 
+;; Custom.
+(custom-set-variables
+ '(custom-enabled-themes (quote (mustang-netrom)))
+ '(custom-safe-themes
+   (quote
+    ("980f08fa0417c3a9cc5ada212bdf5de7b16fc3b499295d100a2277ce46f2e2a2"
+     default)))
+ '(recentf-exclude
+   (quote
+    ("ido.last" ".emacs.d/saveplace.txt" "/var/folders/" "~/Maildir")))
+ '(uniquify-buffer-name-style
+   (quote post-forward-angle-brackets) nil (uniquify)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ )
+
 ;; Recompile all configurations when closing emacs.
 (add-hook 'kill-emacs-hook
-	  (lambda ()
-	    (byte-recompile-directory init-dir 0 t)))
+          (lambda ()
+            (byte-recompile-directory init-dir 0 t)))
 
 ;; Packages setup.
 (require 'package)
@@ -34,10 +51,9 @@
 (require-package 'req-package)
 (require 'req-package)
 (req-package-force load-dir
-		   :defer 1
-		   :init (progn (setq force-load-messages nil)
-				(setq load-dir-debug t);;nil
-				(setq load-dir-recursive t))
-		   :config (progn (load-dir-one init-dir)
-				  (req-package-finish)))
-
+  :defer 1
+  :init (progn (setq force-load-messages nil)
+               (setq load-dir-debug t);;nil
+               (setq load-dir-recursive t))
+  :config (progn (load-dir-one init-dir)
+                 (req-package-finish)))
