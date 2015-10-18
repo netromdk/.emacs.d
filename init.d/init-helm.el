7d3ecd13a6f8789a20125a158829279c5d32562e
Morten Kristensen <msk@nullpointer.dk>
2015-10-18 12:52:57 +0200

    helm: Fuzzy matching for helm-M-x.

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -9,6 +9,7 @@
     (setq helm-exit-idle-delay 0)
     (setq helm-full-frame nil)
     (setq helm-buffers-fuzzy-matching t)
+    (setq helm-M-x-fuzzy-match t)
     (setq helm-ff-file-name-history-use-recentf t)
     (setq helm-split-window-default-side (quote below))
     (setq helm-reuse-last-window-split-state nil)
