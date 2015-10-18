edcff70aea575cd539ec60045a6c245846739535
Morten Kristensen <msk@nullpointer.dk>
2015-10-18 13:00:43 +0200

    helm: Bind "C-x C-f" to helm-find-files.

--- init.d/init-helm.el
+++ init.d/init-helm.el
@@ -25,6 +25,7 @@
     (global-set-key (kbd "M-x") 'helm-M-x)
     (global-set-key (kbd "M-y") 'helm-show-kill-ring)
     (global-set-key (kbd "C-x b") 'helm-mini)
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
 
     ;; Cycle through sources. C-o calls helm-next-source.
     (define-key helm-map (kbd "M-o") 'helm-previous-source)
