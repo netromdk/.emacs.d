592afbc005ac7b32e876ce65240df6a3c673174f
Morten Kristensen <msk@nullpointer.dk>
2015-10-19 23:46:14 +0200

    yas: Using yas-ido-prompt.

--- init.d/init-yasnippet.el
+++ init.d/init-yasnippet.el
@@ -6,6 +6,9 @@
     ;; Add local snippets to override some of the defaults in elpa folder.
     (add-to-list 'yas-snippet-dirs yas-dir)
 
+    (setq yas-prompt-functions
+          '(yas-ido-prompt yas-dropdown-prompt yas-completing-prompt yas-x-prompt yas-no-prompt))
+
     (yas-global-mode 1)
 
     ;; Disable normal tab expansion because it often interferes with
