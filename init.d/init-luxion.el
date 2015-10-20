6fc85c45c9351f4b57796c2a3b3746fab76ccdb2
Morten Kristensen <msk@nullpointer.dk>
2015-10-20 23:26:13 +0200

    lux: Changed wrap-luxion-function and yasnippets.
    Instead of counting to fit the input function line length it is now always 80
    characters long "// *****.." for both lines; before and after. Same for the
    yasnippets, and the position of the last "{" is moved down under "// ***.." so
    that fixing indentation of the buffer doesn't offset them.
    
    Before:
    
    // ****************************
    void test(int foo, float bar) {
    // ****************************
    
    }
    
    After:
    
    // *****************************************************************************
    void test(int foo, float bar)
    // *****************************************************************************
    {
    
    }
    
    If buffer is re-indented the first would look like this:
    
    // ****************************
    void test(int foo, float bar) {
      // ****************************
    
    }
    
    While the new one would stay in place.

--- init.d/init-luxion.el
+++ init.d/init-luxion.el
@@ -1,39 +1,19 @@
 ;;;;;;;;; Luxion related
 
-(defun wrap-luxion-count (start end)
-  "Determines the longest horizontal stretch of the function selected."
-  (progn
-    (goto-char start)
-    (let (max)
-      (progn 
-        (setq max 0)
-        (while (search-forward "\n" end 't)
-          (progn
-            ;; Move back once to get the correct column. The cursor is
-            ;; on the first char on the next line after searching for
-            ;; "\n".
-            (backward-char) 
-            (if (> (current-column) max)
-                (setq max (current-column)))
-            (forward-char))))
-      ;; Subtract three to compensate for "// ".
-      (- max 3))))
-
-;; Luxion: Wraps // ***.. before and after a function (the region
-;; selected). Place region at the start of the function and end it
-;; after "{" on the next line.
+;; Wraps a function with // ***.. before and after (the region selected). Both
+;; inserted lines with have a length fo 80 characters.
 (defun wrap-luxion-function (start end)
- "Put comments around Luxion function."
- (interactive "r")    
- (let (count str)
-   (progn
-     (save-excursion 
-       (setq count (wrap-luxion-count start end))
-       (setq str (concat "// " (make-string count ?*) "\n"))
-       (goto-char end) 
-       (insert str)
-       (goto-char start) 
-       (insert str)))))
+  "Put comments around Luxion function."
+  (interactive "r")
+  (let (count str)
+    (progn
+      (save-excursion
+        (setq count 77) ;; 80 - "// "
+        (setq str (concat "// " (make-string count ?*) "\n"))
+        (goto-char end)
+        (insert str)
+        (goto-char start)
+        (insert str)))))
 
 (global-set-key (kbd "C-M-l") 'wrap-luxion-function)
 
