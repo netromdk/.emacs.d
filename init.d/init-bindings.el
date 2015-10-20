a06929d948db99a6a8d87b6c4551669d394419d2
Morten Kristensen <morten@luxion.com>
2015-10-20 09:40:34 +0200

    Set hippie-expand-try-functions-list to match expectations.

--- init.d/init-bindings.el
+++ init.d/init-bindings.el
@@ -23,6 +23,19 @@
 ;; Using hippie-expand instead of dabbrev-expand.
 (global-set-key (kbd "M-/") 'hippie-expand)
 
+;; Put dabbrev expansions first because it's most often what's expected.
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-expand-all-abbrevs
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
 ;; Jump to last mark and pop the mark (does not affect the mark ring). Easy way
 ;; to get back to a mark.
 (global-set-key (kbd "C-x p") 'pop-to-mark-command)
