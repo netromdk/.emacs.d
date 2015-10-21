9d252531f09ec3b0e429024be4589e536a96b3db
Morten Kristensen <morten@luxion.com>
2015-10-21 13:44:58 +0200

    ws uses fill-column for highlights.

--- init.d/init-whitespace.el
+++ init.d/init-whitespace.el
@@ -1,6 +1,7 @@
 ;; Visualize certain like space at end of line and trailing characters after
 ;; fill column.
 (setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-line-column fill-column)
 
 ;; Enable whitespace for programming modes.
 (add-hook 'prog-mode-hook 'whitespace-mode)
