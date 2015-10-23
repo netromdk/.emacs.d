2a626b35b4ef1de79414118b967336d9b246ca0b
Morten Kristensen <msk@nullpointer.dk>
2015-10-23 20:11:09 +0200

    Removed unnecessary defadvice about a warning to suppress.

--- functions.el
+++ functions.el
@@ -117,14 +117,3 @@
   "Returns non-nil if string STRING starts with PREFIX, otherwise nil."
   (and (>= (length string) (length prefix))
        (string-equal (substring string 0 (length prefix)) prefix)))
-
-;;;;;;;;; WARNINGS
-
-;; Ignore the load-path warning because I want my .emacs.d as I have
-;; it now!
-(defadvice display-warning
-    (around no-warn-.emacs.d-in-load-path (type message &rest unused) activate)
-  "Ignore the warning about the `.emacs.d' directory being in `load-path'."
-  (unless (and (eq type 'initialization)
-               (string-starts-with message "Your `load-path' seems to contain\nyour `.emacs.d' directory"))
-    ad-do-it))
