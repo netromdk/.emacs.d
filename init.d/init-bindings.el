7227d056fb080689aaa91b68e3838c295ef49a9e
Morten Kristensen <msk@nullpointer.dk>
2015-10-25 22:35:45 +0100

    M-p/n does backward/forward-paragraph instead of scrolling line up/down.

--- init.d/init-bindings.el
+++ init.d/init-bindings.el
@@ -1,6 +1,6 @@
 ;; General bindings.
-(global-set-key (kbd "M-n") 'scroll-line-down)
-(global-set-key (kbd "M-p") 'scroll-line-up)
+(global-set-key (kbd "M-p") 'backward-paragraph)
+(global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "C-M-p") 'move-line-up)
 (global-set-key (kbd "C-M-n") 'move-line-down)
 (global-set-key (kbd "M-P") 'previous-user-buffer)
