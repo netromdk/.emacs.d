e34cef35c4f470e5ee9fac87e478ccbfccfa75d9
Morten Kristensen <msk@nullpointer.dk>
2016-03-13 11:47:40 +0100

    magit: Using more default conf. Had old conf.

--- init.d/init-vc.el
+++ init.d/init-vc.el
@@ -3,13 +3,6 @@
 (req-package magit
   :config
   (progn
-    (setq magit-revert-item-confirm t)
-    (setq magit-save-some-buffers t)
-    (setq magit-auto-revert-mode nil) ;; Do _not_ auto-revert!
-    (setq magit-revert-buffers t)
-    (setq magit-last-seen-setup-instructions "1.4.0") ;; Silence latest info.
-    (setq magit-push-always-verify nil) ;; Only ask when upstream is not conf'ed!
-
     ;; Set defaults used by specific operations.
     (setq magit-pull-arguments '("--rebase"))
     (setq magit-cherry-pick-arguments '("-x"))
