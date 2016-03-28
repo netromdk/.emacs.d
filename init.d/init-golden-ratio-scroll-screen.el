54f60a90c7bb82a974273e4b12fc2ac6b70160c9
Morten Kristensen <msk@nullpointer.dk>
2016-03-28 17:23:39 +0200

    Added missing )

--- init.d/init-golden-ratio-scroll-screen.el
+++ init.d/init-golden-ratio-scroll-screen.el
@@ -4,7 +4,7 @@
   :config
   (progn
     (global-set-key [remap scroll-down-command] 'golden-ratio-scroll-screen-down)
-    (global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up))
+    (global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up)))
 
 
 (provide 'init-golden-ratio-scroll-screen)
