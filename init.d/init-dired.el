a3da99061d79780e7bfd52f440171282005028fc
Morten Kristensen <msk@nullpointer.dk>
2016-03-28 17:24:31 +0200

    dired: Show human-readable sizes and auto refresh on changes.

--- init.d/init-dired.el
+++ init.d/init-dired.el
@@ -1,8 +1,14 @@
 (require 'req-package)
 
 (req-package ls-lisp
+  :init
+  ;; Automatically refresh dired buffer on changes.
+  (add-hook 'dired-mode-hook 'auto-revert-mode)
+
   :config
   (progn
+    (setq dired-listing-switches "-lha") ;; Show human-readable sizes.
+
     ;; In dired mode, use Emacs's emulation of "ls" because the system one most
     ;; often doesn't support the "--dired" argument.
     (setq ls-lisp-use-insert-directory-program nil)))
