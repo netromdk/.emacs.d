54320f2528dac8867a4cd6485efab88b711b61c7
Morten Kristensen <msk@nullpointer.dk>
2016-03-28 16:34:14 +0200

    Using golden-ratio-scroll-screen on up/down.

---
+++ init.d/init-golden-ratio-scroll-screen.el
@@ -0,0 +1,10 @@
+(require 'req-package)
+
+(req-package golden-ratio-scroll-screen
+  :config
+  (progn
+    (global-set-key [remap scroll-down-command] 'golden-ratio-scroll-screen-down)
+    (global-set-key [remap scroll-up-command] 'golden-ratio-scroll-screen-up))
+
+
+(provide 'init-golden-ratio-scroll-screen)
