4ab0123b85508586098bda609c3db836da3fae83
Morten Kristensen <morten@luxion.com>
2016-03-29 09:23:40 +0200

    dired: Use new req-package format.

--- init.d/init-dired.el
+++ init.d/init-dired.el
@@ -6,12 +6,11 @@
   (add-hook 'dired-mode-hook 'auto-revert-mode)
 
   :config
-  (progn
-    (setq dired-listing-switches "-lha") ;; Show human-readable sizes.
+  (setq dired-listing-switches "-lha") ;; Show human-readable sizes.
 
-    ;; In dired mode, use Emacs's emulation of "ls" because the system one most
-    ;; often doesn't support the "--dired" argument.
-    (setq ls-lisp-use-insert-directory-program nil)))
+  ;; In dired mode, use Emacs's emulation of "ls" because the system one most
+  ;; often doesn't support the "--dired" argument.
+  (setq ls-lisp-use-insert-directory-program nil))
 
 
 (provide 'init-dired)
