a0da2a8478173e121ff0bcb3f4a552c326d5e3cb
Morten Kristensen <msk@nullpointer.dk>
2016-03-30 23:53:10 +0200

    whitespace: Visualize tab with marks.

--- init.d/init-whitespace.el
+++ init.d/init-whitespace.el
@@ -1,6 +1,6 @@
 ;; Visualize certain like space at end of line and trailing characters after
 ;; fill column.
-(setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-style '(face empty tabs lines-tail trailing tab-mark))
 (setq whitespace-line-column global-fill-column)
 
 (add-hook 'prog-mode-hook 'whitespace-mode)
