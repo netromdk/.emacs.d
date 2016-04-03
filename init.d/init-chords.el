4d79399c598b2a9851461276ca180df178b1fbdb
Morten Kristensen <msk@nullpointer.dk>
2016-04-03 14:27:06 +0200

    chords: Don't use 'qw' because it can clash.
    For instance with "qwerty".

--- init.d/init-chords.el
+++ init.d/init-chords.el
@@ -2,7 +2,6 @@
 
 (req-package key-chord
   :config
-  (key-chord-define-global "qw" 'kill-this-buffer)
   (key-chord-define-global "qq" 'kill-this-buffer)
   (key-chord-define-global "''" "`'\C-b")
   (key-chord-define-global ",," 'indent-for-comment-and-indent)
