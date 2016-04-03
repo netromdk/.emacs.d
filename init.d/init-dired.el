ce692a748311c2cf85fca0675ddd2e2a888d2039
Morten Kristensen <msk@nullpointer.dk>
2016-04-03 20:02:47 +0200

    dired: Better movement in dired buffers.

--- init.d/init-dired.el
+++ init.d/init-dired.el
@@ -14,11 +14,32 @@
 
   (key-chord-define dired-mode-map "qq" 'dired-up-directory)
 
-  (define-key dired-mode-map (kbd "M-o") 'dired-omit-mode)
-
   ;; Unwanted files to be flagged for deletion on dired-flag-garbage-files or "%&".
   (setq dired-garbage-files-regexp
-        "\\(?:\\.\\(?:aux\\|bak\\|dvi\\|log\\|orig\\|rej\\|toc\\|elc\\)\\)\\'"))
+        "\\(?:\\.\\(?:aux\\|bak\\|dvi\\|log\\|orig\\|rej\\|toc\\|elc\\)\\)\\'")
+
+  (define-key dired-mode-map (kbd "k") 'dired-do-delete)
+  (define-key dired-mode-map (kbd "M-o") 'dired-omit-mode)
+
+  ;; C-a goes to start of file entry (column 2).
+  (defun dired-back-to-start-of-files ()
+    (interactive)
+    (backward-char (- (current-column) 2)))
+  (define-key dired-mode-map (kbd "C-a") 'dired-back-to-start-of-files)
+
+  ;; M-< goes to first file, the fourth line.
+  (defun dired-back-to-top ()
+    (interactive)
+    (beginning-of-buffer)
+    (dired-next-line 4))
+  (define-key dired-mode-map (vector 'remap 'beginning-of-buffer) 'dired-back-to-top)
+
+  ;; M-> goes to last file.
+  (defun dired-jump-to-bottom ()
+    (interactive)
+    (end-of-buffer)
+    (dired-next-line -1))
+  (define-key dired-mode-map (vector 'remap 'end-of-buffer) 'dired-jump-to-bottom))
 
 ;; dired-k makes directory listings more readable, adding a bit of color and some git status
 ;; information on files and directories.
