49908f3ea07a9592678b91c378c7ef619b6a8a07
Morten Kristensen <msk@nullpointer.dk>
2016-04-03 13:01:15 +0200

    Put avy into init-search.el

--- init.d/init-search.el
+++ init.d/init-search.el
@@ -9,5 +9,10 @@
   (global-set-key (kbd "C-M-s") 'flx-isearch-forward)
   (global-set-key (kbd "C-M-r") 'flx-isearch-backward))
 
+(req-package avy
+  :config
+  (global-set-key (kbd "C-c 1") 'avy-goto-char)
+  (global-set-key (kbd "C-c 2") 'avy-goto-char-2))
+
 
 (provide 'init-search)
