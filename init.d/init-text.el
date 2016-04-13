82bf3e1ab3596b9942e7860755ada9a3ed4c7568
Morten Kristensen <msk@nullpointer.dk>
2016-04-13 20:17:41 +0200

    text: Don't auto-enable orgtbl and orgstruct modes.
    I don't really use them anyway.

--- init.d/init-text.el
+++ init.d/init-text.el
@@ -1,8 +1,4 @@
 ;; Set auto-fill-mode and org minor modes for lists and tables.
-(add-hook 'text-mode-hook
-          (lambda ()
-            (auto-fill-mode t)
-            (orgstruct-mode t)
-            (orgtbl-mode t)))
+(add-hook 'text-mode-hook (lambda () (auto-fill-mode t)))
 
 (provide 'init-text)
