81c64774328391161c6d2dd429be897127d100b3
Morten Kristensen <morten@luxion.com>
2016-04-14 09:17:10 +0200

    Turn on smerge-mode in files with the markers in them.

---
+++ init.d/init-smerge.el
@@ -0,0 +1,13 @@
+(require 'req-package)
+
+;; Turn on smerge-mode when opening a file with the markers in them.
+(defun sm-try-smerge ()
+  (save-excursion
+    (goto-char (point-min))
+    (when (re-search-forward "^<<<<<<< " nil t)
+      (smerge-mode 1))))
+
+(add-hook 'find-file-hook 'sm-try-smerge)
+
+
+(provide 'init-smerge)
