a0c6b007ef9178c7dacba531745a302d2bdfdaab
Morten Kristensen <msk@nullpointer.dk>
2016-04-22 11:15:41 +0200

    Hippie: Using lisp expansions for lisp modes.

--- init.d/init-bindings.el
+++ init.d/init-bindings.el
@@ -28,21 +28,5 @@
 (global-set-key (kbd "C-o") 'prelude-smart-open-line)
 (global-set-key (kbd "M-o") 'open-line) ;; Default way.
 
-;; Using hippie-expand instead of dabbrev-expand.
-(global-set-key (kbd "M-/") 'hippie-expand)
-
-;; Put dabbrev expansions first because it's most often what's expected.
-(setq hippie-expand-try-functions-list
-      '(try-expand-dabbrev
-        try-expand-dabbrev-all-buffers
-        try-expand-dabbrev-from-kill
-        try-expand-all-abbrevs
-        try-complete-file-name-partially
-        try-complete-file-name
-        try-expand-list
-        try-expand-line
-        try-complete-lisp-symbol-partially
-        try-complete-lisp-symbol))
-
 
 (provide 'init-bindings)
---
+++ init.d/init-hippie-expand.el
@@ -0,0 +1,37 @@
+(require 'req-package)
+
+;; Using hippie-expand instead of dabbrev-expand.
+(global-set-key (kbd "M-/") 'hippie-expand)
+
+;; Put dabbrev expansions first because it's most often what's expected.
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-expand-all-abbrevs
+        try-expand-whole-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-list
+        try-expand-line))
+
+;; Enable the lisp expansion functions for lisp modes.
+(add-to-multiple-hooks
+ (lambda ()
+   (setq hippie-expand-try-functions-list
+         '(try-complete-lisp-symbol-partially
+           try-complete-lisp-symbol
+           try-expand-dabbrev
+           try-expand-dabbrev-all-buffers
+           try-expand-dabbrev-from-kill
+           try-expand-all-abbrevs
+           try-expand-whole-kill
+           try-complete-file-name-partially
+           try-complete-file-name
+           try-expand-list
+           try-expand-line)))
+ '(emacs-lisp-mode-hook
+   lisp-interaction-mode-hook))
+
+
+(provide 'init-hippie-expand)
