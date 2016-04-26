66a743ea0ba66e744264a2ea185cab1637ee9cb4
Morten Kristensen <morten@luxion.com>
2016-04-26 13:29:29 +0200

    hippie: Set lisp expand locally only.
    Otherwise after adding the lisp expansion functions and switcing to non-lisp files they would persist.

--- init.d/init-hippie-expand.el
+++ init.d/init-hippie-expand.el
@@ -16,18 +16,18 @@
 ;; Enable the lisp expansion functions for lisp modes.
 (add-to-multiple-hooks
  (lambda ()
-   (setq hippie-expand-try-functions-list
-         '(try-complete-lisp-symbol-partially
-           try-complete-lisp-symbol
-           try-expand-dabbrev
-           try-expand-dabbrev-all-buffers
-           try-expand-dabbrev-from-kill
-           try-expand-all-abbrevs
-           try-expand-whole-kill
-           try-complete-file-name-partially
-           try-complete-file-name
-           try-expand-list
-           try-expand-line)))
+   (setq-local hippie-expand-try-functions-list
+               '(try-complete-lisp-symbol-partially
+                 try-complete-lisp-symbol
+                 try-expand-dabbrev
+                 try-expand-dabbrev-all-buffers
+                 try-expand-dabbrev-from-kill
+                 try-expand-all-abbrevs
+                 try-expand-whole-kill
+                 try-complete-file-name-partially
+                 try-complete-file-name
+                 try-expand-list
+                 try-expand-line)))
  '(emacs-lisp-mode-hook
    lisp-interaction-mode-hook))
 
