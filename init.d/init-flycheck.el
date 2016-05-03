1337c713438dad121c187d5ee8d6d945f453084a
Morten Kristensen <morten@luxion.com>
2016-05-03 14:28:44 +0200

    flycheck: Enabled again and disabling elisp checkdoc.

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -3,20 +3,21 @@
 ;;; Code:
 (require 'req-package)
 
-;; Disabled for now because it doesn't always work as intended and can be annoying..
-;; (req-package flycheck
-;;   :config
-;;   (progn
-;;     ;; C++11
-;;     (add-hook 'c++-mode-hook
-;;               (lambda ()
-;;                 (progn
-;;                   (setq flycheck-clang-language-standard "c++11")
-;;                   (setq flycheck-clang-standard-library "libc++")
-;;                   (setq flycheck-gcc-language-standard "c++11"))))
+(req-package flycheck
+  :config
+  (progn
+    ;; C++11
+    (add-hook 'c++-mode-hook
+              (lambda ()
+                (progn
+                  (setq flycheck-clang-language-standard "c++11")
+                  (setq flycheck-clang-standard-library "libc++")
+                  (setq flycheck-gcc-language-standard "c++11"))))
 
-;;     (add-hook 'prog-mode-hook 'flycheck-mode)))
+    ;; Disable elisp checkdoc because it's annoying!
+    (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
+    (add-hook 'prog-mode-hook 'flycheck-mode)))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
