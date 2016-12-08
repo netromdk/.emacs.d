23d56d199e907874408bdabd325759834e3dc93d
Morten Kristensen <msk@nullpointer.dk>
2016-12-08 15:29:05 +0100

    mc: using mc/mark-all-dwim on "C-c m" because it's more intuitive
    It marks first in scope by word, so it doesn't match what you don't want it to, and clicking "C-c m"
    again will match the entire file.

--- init.d/init-multiple-cursors.el
+++ init.d/init-multiple-cursors.el
@@ -5,7 +5,8 @@
   :config
   (global-set-key (kbd "C-c n") 'mc/mark-next-like-this)
   (global-set-key (kbd "C-c p") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c m") 'mc/mark-all-like-this))
+  (global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
+  (global-set-key (kbd "C-c m") 'mc/mark-all-dwim))
 
 
 (provide 'init-multiple-cursors)
