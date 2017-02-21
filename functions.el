390be6d450b02204767ff2605c043c212a6dfd37
Morten Kristensen <msk@nullpointer.dk>
2017-02-21 22:28:34 +0100

    Cleanup

--- functions.el
+++ functions.el
@@ -139,9 +139,12 @@ compile to .elc if not already present first."
 ;; Set exec path to be the same as the one from the shell
 (defun set-exec-path-from-shell-path ()
   "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
-  This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  This is particularly useful under Mac OSX, where GUI apps are
+  not started from a shell."
   (interactive)
-  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+  (let ((path-from-shell
+         (replace-regexp-in-string "[ \t\n]*$" ""
+                                   (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
     (setenv "PATH" path-from-shell)
     (setq exec-path (split-string path-from-shell path-separator))))
 (set-exec-path-from-shell-path)
