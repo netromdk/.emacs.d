4c44a25cfd35ef7f4cd42df841b322a0a52a9fdf
Morten Kristensen <msk@nullpointer.dk>
2017-03-06 21:57:16 +0100

    Using fix-word to override: M-u, M-l, and M-c

--- init.d/init-editing.el
+++ init.d/init-editing.el
@@ -1,6 +1,6 @@
-;; Multiple cursors
 (require 'req-package)
 
+;; Multiple cursors
 (req-package multiple-cursors
   :config
   (global-set-key (kbd "C-c n") 'mc/mark-next-like-this)
@@ -8,5 +8,12 @@
   (global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
   (global-set-key (kbd "C-c m") 'mc/mark-all-dwim))
 
+;; Convenient word transformations.
+(req-package fix-word
+  :config
+  (global-set-key (kbd "M-u") 'fix-word-upcase)
+  (global-set-key (kbd "M-l") 'fix-word-downcase)
+  (global-set-key (kbd "M-c") 'fix-word-capitalize))
 
-(provide 'init-multiple-cursors)
+
+(provide 'init-editing)
