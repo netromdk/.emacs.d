50df8efe790c4298a36fb4e703279176dc35adb7
Morten Kristensen <morten@luxion.com>
2017-09-13 09:53:37 +0200

    copy-as-format: "C-c w" bound to copying-hydra/body

--- init.d/init-copying.el
+++ init.d/init-copying.el
@@ -2,12 +2,25 @@
 
 ;; Convenience for formatting things to be copied and inserted elsewhere, like code blocks.
 (req-package copy-as-format
+  :require hydra
   :config
-  (global-set-key (kbd "C-c w s") 'copy-as-format-slack)
-  (global-set-key (kbd "C-c w m") 'copy-as-format-markdown)
-  (global-set-key (kbd "C-c w g") 'copy-as-format-github)
-  (global-set-key (kbd "C-c w l") 'copy-as-format-gitlab)
-  (global-set-key (kbd "C-c w h") 'copy-as-format-html)
-  (global-set-key (kbd "C-c w j") 'copy-as-format-jira))
+
+  (defhydra copying-hydra (:color blue :columns 5 :idle 1)
+    "Copy as format"
+    ("s" copy-as-format-slack "slack")
+    ("j" copy-as-format-jira "jira")
+    ("m" copy-as-format-markdown "markdown")
+    ("g" copy-as-format-github "github")
+    ("l" copy-as-format-gitlab "gitlab")
+    ("h" copy-as-format-html "html")
+    ("o" copy-as-format-org-mode "org-mode")
+    ("w" copy-as-format-mediawiki "mediawiki")
+    ("b" copy-as-format-bitbucket "bitbucket")
+    ("r" copy-as-format-rst "rst")
+    ("p" copy-as-format-pod "pod")
+    ("c" copy-as-format-hipchat "hipchat")
+    ("d" copy-as-format-disqus "discus"))
+
+  (global-set-key (kbd "C-c w") 'copying-hydra/body))
 
 (provide 'init-copying)
