98ab32fda0737e40c4b3c2b01ce9422e88f708a4
Morten Kristensen <morten@luxion.com>
2017-09-28 11:11:04 +0200

    magit: Bring back "fullscreen" status buffer

--- init.d/init-vc.el
+++ init.d/init-vc.el
@@ -9,6 +9,12 @@
         magit-diff-arguments '("-U3" "--stat" "--no-ext-diff")
         magit-fetch-arguments '("--prune"))
 
+  ;; Show status full screen.
+  (defadvice magit-status (around magit-fullscreen activate)
+    (window-configuration-to-register :magit-fullscreen)
+    ad-do-it
+    (delete-other-windows))
+
   (global-set-key (kbd "C-x g") 'magit-status))
 
 ;; Highlight uncommitted changes/additions/deletions in the fringe.
