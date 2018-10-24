563321fbe3992af89a59d035dedc43548eff951d
Morten Kristensen <msk@nullpointer.dk>
2018-10-24 10:26:31 +0200

    Merged hydra configurations

--- init.d/init-prog.el
+++ init.d/init-prog.el
@@ -75,7 +75,20 @@ in compilation mode."
     (user-error nil)))
 
 (req-package hydra
+  :require helm
   :config
+  ;; Easier cycling of yanking.
+  (defhydra yank-pop-hydra ()
+    "yank"
+    ("C-y" yank nil)
+    ("M-y" yank-pop nil)
+    ("y" (yank-pop 1) "next")
+    ("Y" (yank-pop -1) "prev")
+    ("l" helm-show-kill-ring "list" :color blue))
+
+  (global-set-key (kbd "M-y") #'yank-pop-hydra/yank-pop)
+  (global-set-key (kbd "C-y") #'yank-pop-hydra/yank)
+
   (defhydra compilation-hydra (:columns 4)
     "
 Command: %(msk/compilation-command-string)
