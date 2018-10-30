f1aeddfafc1e20b454f6d6000d350b6cfdb6ae5c
Morten Kristensen <msk@nullpointer.dk>
2018-10-30 13:59:15 +0100

    Cleaned up company setup

--- init.d/init-company.el
+++ init.d/init-company.el
@@ -10,19 +10,7 @@
 
   (global-company-mode 1)
 
-  ;; Use C-tab globally for company-complete but use C-tab for gtags
-  ;; completion locally for C like modes, but in that case keep C-return
-  ;; with the default completion.
-  (global-set-key (kbd "C-<tab>") 'company-complete)
-
-  ;; Disabled because irony is working better for now.
-  ;; (add-hook 'c-mode-common-hook
-  ;;           (lambda ()
-  ;;             (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
-  ;;               (local-set-key (kbd "C-<tab>") 'company-gtags)
-  ;;               (local-set-key (kbd "C-<return>") 'company-complete))))
-  )
-
+  (global-set-key (kbd "C-<tab>") 'company-complete))
 
 (req-package company-flx
   :require company
