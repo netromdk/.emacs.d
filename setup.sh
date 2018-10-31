4f2fa0e199bf8c1d5c08b9d089454094daedcba8
Morten Kristensen <msk@nullpointer.dk>
2018-10-31 09:28:28 +0100

    setup.sh easily installs local dependencies required by the config
    Currently:
    * cquery
    * the_silver_searcher (ag)
    * cppcheck
    * python-language-server (pyls)

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+OS=$(uname -s)
+case "${OS}" in
+  Darwin)
+    brew install cquery
+    brew install the_silver_searcher
+    brew install cppcheck
+    pip install python-language-server
+    ;;
+
+  *)
+    echo "Unknown how to setup deps for '${OS}'!"
+    ;;
+esac
