43b2ee1afb2c2dc3ed67bf6f06899463e74d5a3c
Morten Kristensen <msk@nullpointer.dk>
2018-11-02 22:54:08 +0100

    Automatically tail log files but don't interpret as text
    A side-effect of a text-mode file is that it will enable spell checking, and that can be quite
    costly for huge files if not intended.

--- general.el
+++ general.el
@@ -97,10 +97,7 @@
 (setq auto-revert-verbose nil)            ; Don't announce when buffer is reverted.
 (global-auto-revert-mode t)
 
-;; Log files should automatically update on changes and be interpreted as text file.
+;; Log files should automatically update on changes.
 (setq auto-mode-alist
-      (append '(("\\.log$" .
-                 (lambda ()
-                   (text-mode)
-                   (auto-revert-tail-mode))))
+      (append '(("\\.log$" . (lambda () (auto-revert-tail-mode))))
               auto-mode-alist))
