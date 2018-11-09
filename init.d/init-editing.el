17f23602d208548c6382cc7e55d99f2026dfbc19
Morten Kristensen <msk@nullpointer.dk>
2018-11-09 14:14:05 +0100

    Removed duplicate config of mc

--- init.d/init-editing.el
+++ init.d/init-editing.el
@@ -1,13 +1,5 @@
 (require 'req-package)
 
-;; Multiple cursors
-(req-package multiple-cursors
-  :config
-  (global-set-key (kbd "C-c n") 'mc/mark-next-like-this)
-  (global-set-key (kbd "C-c p") 'mc/mark-previous-like-this)
-  (global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
-  (global-set-key (kbd "C-c m") 'mc/mark-all-dwim))
-
 ;; Convenient word transformations.
 (req-package fix-word
   :config
