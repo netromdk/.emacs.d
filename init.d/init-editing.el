762c594c5c0a0de5c16b2b3e0940dfff17315916
Morten Kristensen <msk@nullpointer.dk>
2018-11-09 14:16:34 +0100

    Toggleable filling with "M-q" using unfill package

--- init.d/init-editing.el
+++ init.d/init-editing.el
@@ -7,5 +7,10 @@
   (global-set-key (kbd "M-l") 'fix-word-downcase)
   (global-set-key (kbd "M-c") 'fix-word-capitalize))
 
+;; Better fill/unfill that is toggleable.
+(req-package unfill
+  :config
+  (global-set-key (kbd "M-q") 'unfill-toggle))
+
 
 (provide 'init-editing)
