110bf7075dced9ee285dff0e2264dabfd929776a
Morten Kristensen <me@mortens.dev>
2019-03-09 00:43:55 +0100

    magit: Protect against accidental pushes to upstream/push remote

--- init.d/init-vc.el
+++ init.d/init-vc.el
@@ -15,7 +15,41 @@
     ad-do-it
     (delete-other-windows))
 
-  (global-set-key (kbd "C-x g") 'magit-status))
+  ;; Bindings.
+  (global-set-key (kbd "C-x g") 'magit-status)
+
+  ;; Protect against accident pushes to upstream/push remote.
+  (defadvice magit-push-current-to-upstream
+      (around my-protect-accidental-magit-push-current-to-upstream)
+    "Protect against accidental push to upstream.
+
+Causes `magit-git-push' to ask the user for confirmation first."
+    (let ((my-magit-ask-before-push t))
+      ad-do-it))
+
+  (defadvice magit-push-current-to-pushremote
+      (around my-protect-accidental-magit-push-current-to-pushremote)
+    "Protect against accidental push to push remote.
+
+Causes `magit-git-push' to ask the user for confirmation first."
+    (let ((my-magit-ask-before-push t))
+      ad-do-it))
+
+  (defadvice magit-git-push (around my-protect-accidental-magit-git-push)
+    "Maybe ask the user for confirmation before pushing.
+
+Advice to `magit-push-current-to-upstream' triggers this query."
+    (if (bound-and-true-p my-magit-ask-before-push)
+        ;; Arglist is (BRANCH TARGET ARGS)
+        (if (yes-or-no-p (format "Push %s branch upstream to %s? "
+                                 (ad-get-arg 0) (ad-get-arg 1)))
+            ad-do-it
+          (error "Push to upstream aborted by user"))
+      ad-do-it))
+
+  (ad-activate 'magit-push-current-to-upstream)
+  (ad-activate 'magit-push-current-to-pushremote)
+  (ad-activate 'magit-git-push))
 
 ;; Highlight uncommitted changes/additions/deletions in the fringe.
 (req-package diff-hl
