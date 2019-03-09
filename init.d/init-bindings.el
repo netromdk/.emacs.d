72d37720fd5038fb3d6ca8656c175996bddb86e4
Morten Kristensen <me@mortens.dev>
2019-03-09 11:59:27 +0100

    Using beacon to always know where the cursor is

--- init.d/init-bindings.el
+++ init.d/init-bindings.el
@@ -1,6 +1,27 @@
+(require 'req-package)
+
+(req-package beacon
+  :config
+  (setq beacon-blink-delay 0.1
+        beacon-blink-duration 0.3
+        beacon-color "#b1d631")
+
+  (defun backward-paragraph-blink ()
+    (interactive)
+    (backward-paragraph)
+    (beacon-blink))
+
+  (defun forward-paragraph-blink ()
+    (interactive)
+    (forward-paragraph)
+    (beacon-blink))
+
+  (global-set-key (kbd "M-p") 'backward-paragraph-blink)
+  (global-set-key (kbd "M-n") 'forward-paragraph-blink)
+
+  (beacon-mode 1))
+
 ;; General bindings.
-(global-set-key (kbd "M-p") 'backward-paragraph)
-(global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "C-c o")
                 '(lambda () (interactive) (ff-find-other-file nil t)))
 (global-set-key (kbd "C-.") 'repeat)
