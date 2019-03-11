db245c5796a7d831f74a2b367ca4cb19158267bd
Morten Kristensen <msk@nullpointer.dk>
2019-03-11 14:11:45 +0100

    Enable flycheck before lsp-ui in order to load correctly

--- init.el
+++ init.el
@@ -1175,6 +1175,79 @@ in compilation mode."
     (read-string "Xref find apropos of: " (thing-at-point 'symbol))))
   (xref-find-apropos pattern))
 
+(use-package flycheck
+  :requires helm-flycheck hydra flycheck-pycheckers flycheck-inline
+  :config
+  (progn
+    ;; C++11
+    (add-hook 'c++-mode-hook
+              (lambda ()
+                (progn
+                  (setq flycheck-clang-language-standard "c++14"
+                        flycheck-clang-standard-library "libc++"
+                        flycheck-gcc-language-standard "c++14"
+                        flycheck-cppcheck-standards '("c++14")
+                        flycheck-cppcheck-inconclusive t
+                        flycheck-cppcheck-checks '("all")
+
+                        ;; Ignore "no explicit constructor" because often you don't want it to be
+                        ;; explicit and in general it's annoying.
+                        flycheck-cppcheck-suppressions '("noExplicitConstructor")))))
+
+    ;; Disable elisp checkdoc because it's annoying, and clang/gcc because they never know the
+    ;; includes anyway!
+    (setq-default flycheck-disabled-checkers
+                  '(emacs-lisp-checkdoc c/c++-clang c/c++-gcc))
+
+    (defalias 'fcn 'flycheck-next-error)
+    (defalias 'hf 'helm-flycheck)
+
+    (add-hook 'prog-mode-hook 'flycheck-mode)
+
+    ;; Enable inline errors/warnings/info etc.
+    (flycheck-inline-mode)
+
+    ;; Navigate flycheck errors more easily.
+    (defhydra flycheck-hydra
+      (:pre  (flycheck-list-errors)
+             :post (quit-windows-on "*Flycheck errors*")
+             :hint nil)
+      "Errors"
+      ("f"  flycheck-error-list-set-filter                            "Filter")
+      ("j"  flycheck-next-error                                       "Next")
+      ("k"  flycheck-previous-error                                   "Previous")
+      ("gg" flycheck-first-error                                      "First")
+      ("G"  (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
+      ("q"  nil))))
+
+(use-package flycheck-inline)
+
+;; Requires local dependencies:
+;;   pip install flake8 bandit
+(use-package flycheck-pycheckers
+  :config
+  (setq flycheck-pycheckers-checkers '(flake8 bandit)
+        flycheck-pycheckers-ignore-codes
+        '("C0411" "C0413" "C0103" "C0111" "W0142" "W0201" "W0232" "W0403" "W0511" "E1002" "E1101"
+          "E1103" "R0201" "R0801" "R0903" "R0904" "R0914" "W503" "W504"
+          ;; flake8
+          "E111" "E114" "E121" "E126" "E127" "E221" "E241" "E302" "E305"
+          ;; bandit
+          "B101" "B322")
+        flycheck-pycheckers-max-line-length 100
+        flycheck-pycheckers-multi-thread "true")
+
+  (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
+
+  ;; Ensure that the correct python checker is chosen.
+  (add-hook 'python-mode-hook (lambda () (flycheck-select-checker 'python-pycheckers))))
+
+;; Requires Rust to be installed locally: install rustup
+(use-package flycheck-rust
+  :requires flycheck
+  :config
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
 ;; Language Server Protocol
 
 ;; Language support:
@@ -1202,7 +1275,6 @@ in compilation mode."
 ;; 4. And while inside "~/.composer/" executing:
 ;;    composer require felixfbecker/language-server
 ;;    composer run-script --working-dir=vendor/felixfbecker/language-server parse-stubs
-
 
 (use-package lsp-mode
   :requires hydra
@@ -1318,79 +1390,6 @@ in compilation mode."
         (lsp)
       (user-error nil)))
   (add-hook 'c++-mode-hook #'msk/cquery-enable))
-
-(use-package flycheck
-  :requires helm-flycheck hydra flycheck-pycheckers flycheck-inline
-  :config
-  (progn
-    ;; C++11
-    (add-hook 'c++-mode-hook
-              (lambda ()
-                (progn
-                  (setq flycheck-clang-language-standard "c++14"
-                        flycheck-clang-standard-library "libc++"
-                        flycheck-gcc-language-standard "c++14"
-                        flycheck-cppcheck-standards '("c++14")
-                        flycheck-cppcheck-inconclusive t
-                        flycheck-cppcheck-checks '("all")
-
-                        ;; Ignore "no explicit constructor" because often you don't want it to be
-                        ;; explicit and in general it's annoying.
-                        flycheck-cppcheck-suppressions '("noExplicitConstructor")))))
-
-    ;; Disable elisp checkdoc because it's annoying, and clang/gcc because they never know the
-    ;; includes anyway!
-    (setq-default flycheck-disabled-checkers
-                  '(emacs-lisp-checkdoc c/c++-clang c/c++-gcc))
-
-    (defalias 'fcn 'flycheck-next-error)
-    (defalias 'hf 'helm-flycheck)
-
-    (add-hook 'prog-mode-hook 'flycheck-mode)
-
-    ;; Enable inline errors/warnings/info etc.
-    (flycheck-inline-mode)
-
-    ;; Navigate flycheck errors more easily.
-    (defhydra flycheck-hydra
-      (:pre  (flycheck-list-errors)
-             :post (quit-windows-on "*Flycheck errors*")
-             :hint nil)
-      "Errors"
-      ("f"  flycheck-error-list-set-filter                            "Filter")
-      ("j"  flycheck-next-error                                       "Next")
-      ("k"  flycheck-previous-error                                   "Previous")
-      ("gg" flycheck-first-error                                      "First")
-      ("G"  (progn (goto-char (point-max)) (flycheck-previous-error)) "Last")
-      ("q"  nil))))
-
-(use-package flycheck-inline)
-
-;; Requires local dependencies:
-;;   pip install flake8 bandit
-(use-package flycheck-pycheckers
-  :config
-  (setq flycheck-pycheckers-checkers '(flake8 bandit)
-        flycheck-pycheckers-ignore-codes
-        '("C0411" "C0413" "C0103" "C0111" "W0142" "W0201" "W0232" "W0403" "W0511" "E1002" "E1101"
-          "E1103" "R0201" "R0801" "R0903" "R0904" "R0914" "W503" "W504"
-          ;; flake8
-          "E111" "E114" "E121" "E126" "E127" "E221" "E241" "E302" "E305"
-          ;; bandit
-          "B101" "B322")
-        flycheck-pycheckers-max-line-length 100
-        flycheck-pycheckers-multi-thread "true")
-
-  (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
-
-  ;; Ensure that the correct python checker is chosen.
-  (add-hook 'python-mode-hook (lambda () (flycheck-select-checker 'python-pycheckers))))
-
-;; Requires Rust to be installed locally: install rustup
-(use-package flycheck-rust
-  :requires flycheck
-  :config
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
 
 ;; Turn on smerge-mode when opening a file with the markers in them.
 (defun sm-try-smerge ()
