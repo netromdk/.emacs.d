19f271b61f36536549b12f9d9d1bc8fce4eed40e
Morten Kristensen <msk@nullpointer.dk>
2019-04-15 14:23:27 +0200

    [magit] Removed accidental push advice that aren't working anymore

--- init.el
+++ init.el
@@ -282,39 +282,6 @@
     ad-do-it
     (delete-other-windows))
 
-  ;; Protect against accident pushes to upstream/push remote.
-  (defadvice magit-push-current-to-upstream
-      (around my-protect-accidental-magit-push-current-to-upstream)
-    "Protect against accidental push to upstream.
-
-Causes `magit-git-push' to ask the user for confirmation first."
-    (let ((my-magit-ask-before-push t))
-      ad-do-it))
-
-  (defadvice magit-push-current-to-pushremote
-      (around my-protect-accidental-magit-push-current-to-pushremote)
-    "Protect against accidental push to push remote.
-
-Causes `magit-git-push' to ask the user for confirmation first."
-    (let ((my-magit-ask-before-push t))
-      ad-do-it))
-
-  (defadvice magit-git-push (around my-protect-accidental-magit-git-push)
-    "Maybe ask the user for confirmation before pushing.
-
-Advice to `magit-push-current-to-upstream' triggers this query."
-    (if (bound-and-true-p my-magit-ask-before-push)
-        ;; Arglist is (BRANCH TARGET ARGS)
-        (if (yes-or-no-p (format "Push %s branch upstream to %s? "
-                                 (ad-get-arg 0) (ad-get-arg 1)))
-            ad-do-it
-          (error "Push to upstream aborted by user"))
-      ad-do-it))
-
-  (ad-activate 'magit-push-current-to-upstream)
-  (ad-activate 'magit-push-current-to-pushremote)
-  (ad-activate 'magit-git-push)
-
   ;; Cycle between how much info is shown in the margin: author + date, date, none.
   (defun magit-cycle-margin ()
     "Cycle visibility of the Magit margin.
