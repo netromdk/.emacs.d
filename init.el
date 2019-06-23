537561c590c370dc18b7a6f06f2d8c1eeef65040
Morten Kristensen <me@mortens.dev>
2019-06-23 13:25:31 +0200

    Don't show empty lines at the end in whitespace mode
    It is sometimes incorrect and very annoying to look at.

--- init.el
+++ init.el
@@ -698,7 +698,7 @@ in compilation mode."
 
 ;; Visualize certain like space at end of line and trailing characters after
 ;; fill column.
-(setq whitespace-style '(face empty tabs lines-tail trailing tab-mark))
+(setq whitespace-style '(face tabs lines-tail trailing tab-mark))
 (setq whitespace-line-column global-fill-column)
 
 (add-hook 'prog-mode-hook 'whitespace-mode)
