d5c90c406c3cbd6169e8730b588aafd2d94581df
Morten Kristensen <me@mortens.dev>
2022-07-04 22:45:55 +0200

    [orderless] regexp default, ! skips, ~ flex
    Use orderless for all completion, defaulting to regexp style.
    If pattern ends with `~' then use flex style.
    If pattern starts with `!' then exclude results using literal style.

--- init.el
+++ init.el
@@ -341,7 +341,23 @@
 
 (use-package orderless
   :config
-  (setq completion-styles '(orderless)))
+  (defun flex-if-twiddle (pattern _index _total)
+    (when (string-suffix-p "~" pattern)
+      `(orderless-flex . ,(substring pattern 0 -1))))
+
+  (defun without-if-bang (pattern _index _total)
+    (cond
+     ((equal "!" pattern)
+      '(orderless-literal . ""))
+     ((string-prefix-p "!" pattern)
+      `(orderless-without-literal . ,(substring pattern 1)))))
+
+  ;; Use orderless for all completion, defaulting to regexp style.
+  ;; If pattern ends with `~' then use flex style.
+  ;; If pattern starts with `!' then exclude results using literal style.
+  (setq completion-styles '(orderless)
+        orderless-matching-styles '(orderless-regexp)
+        orderless-style-dispatchers '(flex-if-twiddle without-if-bang)))
 
 (use-package prescient
   :config
