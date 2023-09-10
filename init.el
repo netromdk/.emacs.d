ccbcff17ff078e0641265b51dd27d98f4d851496
Morten Kristensen <me@mortens.dev>
2023-09-10 11:08:30 +0200

    [linux] Removed deprecated usage of `set-default-font`

--- init.el
+++ init.el
@@ -222,9 +222,6 @@
 
 ;;;;; Linux / X11 Setup ;;;;;
 
-(if (eq window-system 'x)
-    (set-default-font "-misc-fixed-medium-r-normal--13-120-75-75-c-70-iso8859-15"))
-
 ;;;;; Shell ;;;;;
 
 (use-package exec-path-from-shell
