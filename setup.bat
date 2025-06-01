cd2754850902ef255a024d39221cad62a8771706
Morten Kristensen <me@mortens.dev>
2025-06-01 09:18:40 +0200

    setup.bat asks to run as admin + install llvm
    This is to get clangd and clang-format.

--- setup.bat
+++ setup.bat
@@ -1,2 +1,11 @@
-choco install ripgrep shellcheck hunspell.portable
+@echo off
+:: Check for admin rights
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+  powershell -Command "Start-Process '%~f0' -Verb RunAs"
+  exit /b
+)
+
+:: llvm also contains clangd and clang-format.
+choco install -y ripgrep shellcheck hunspell.portable llvm
 pip3 install python-lsp-server flake8 bandit
